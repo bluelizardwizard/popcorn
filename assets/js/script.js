f577ec358921471d871a0e86e4b845535fa9da00
bluelizardwizard <lucy.farwer@haw-hamburg.de>
2025-11-12 22:16:01 +0100

    Change pop-ing logic to be based on classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,12 @@
 const shape = document.getElementById('shape');
 const clickSound = document.getElementById('clickSound');
 
-let should_pop_flag = true;
-
 shape.addEventListener('click', () => {
   // Sound abspielen
   clickSound.currentTime = 0; // springt an den Anfang, falls mehrfach geklickt
-  if (should_pop_flag) {
+  if (shape.classList.contains("kernel")) {
     clickSound.play();
     should_pop_flag = false;
-  } else {
-    should_pop_flag = true;
   }
 
   // Form umschalten
