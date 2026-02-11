300d38e59f86c46072b695ad11800aac46e85743
bluelizardwizard <lucy.farwer@haw-hamburg.de>
2026-02-11 23:35:30 +0100

    added custom humongus cursor and pointer thanks to me brother jacub

--- assets/js/cookie_script.js
+++ assets/js/cookie_script.js
@@ -2,6 +2,33 @@ const container = document.querySelector('.container');
 const sound = document.getElementById('click-sound');
 const sound2 = document.getElementById('sound2'); // added line
 
+function mouse_evt_handler(event) {
+    cursor.style.left = String(event.clientX - 31) + "px";
+    cursor.style.top = String(event.clientY - 4) + "px";
+
+    let flag = false;
+    document.querySelectorAll(".popcorn_container").forEach(element => {
+        var rect = element.getBoundingClientRect();
+        let x = rect.left + ((rect.right - rect.left) / 2);
+        let y = rect.top + ((rect.bottom - rect.top) / 2);
+
+        let x_differance = Math.abs(event.clientX - x);
+        let y_differance = Math.abs(event.clientY - y);
+
+        let distance = Math.sqrt(x_differance**2 + y_differance**2);
+        if (distance <= ((rect.right - rect.left) * 0.6)) {
+            flag = true;
+        }
+    });
+    if (!flag) {
+        cursor_image.src = "assets/media/cursor_centered.png"
+    } else {
+        cursor_image.src = "assets/media/pointer_centered.png"
+    }
+}
+
+document.onmousemove = mouse_evt_handler;
+
 container.addEventListener('click', () => {
   if (!container.classList.contains('open')) {
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,44 @@ document.querySelectorAll(".popcorn_container").forEach((el) => {
 
 const pop_up_div = document.getElementById("pop_up");
 
+function mouse_evt_handler(event) {
+    cursor.style.left = String(event.clientX - 31) + "px";
+    cursor.style.top = String(event.clientY - 4) + "px";
+
+    let flag = false;
+    document.querySelectorAll(".popcorn_container").forEach(element => {
+        var rect = element.getBoundingClientRect();
+        let x = rect.left + ((rect.right - rect.left) / 2);
+        let y = rect.top + ((rect.bottom - rect.top) / 2);
+
+        let x_differance = Math.abs(event.clientX - x);
+        let y_differance = Math.abs(event.clientY - y);
+
+        let distance = Math.sqrt(x_differance**2 + y_differance**2);
+        if (distance <= ((rect.right - rect.left) * 0.6)) {
+            flag = true;
+        }
+    });
+    var rect = pop_up.getBoundingClientRect();
+    let x = rect.left + ((rect.right - rect.left) / 2);
+    let y = rect.top + ((rect.bottom - rect.top) / 2);
+
+    let x_differance = Math.abs(event.clientX - x);
+    let y_differance = Math.abs(event.clientY - y);
+
+    let distance = Math.sqrt(x_differance**2 + y_differance**2);
+    if (distance <= ((rect.right - rect.left) * 0.4)) {
+        flag = true;
+    }
+    if (!flag) {
+        cursor_image.src = "assets/media/cursor_centered.png"
+    } else {
+        cursor_image.src = "assets/media/pointer_centered.png"
+    }
+}
+
+document.onmousemove = mouse_evt_handler;
+
 let special_index = Math.floor(Math.random() * 17); // Chane to number of popcorns here
 let i = 0;
 document.querySelectorAll(".zu").forEach((el) => {
